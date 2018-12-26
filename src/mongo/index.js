160c9628fd2fe85275940d6fb5824b77661ee009
Michael Nasser <bignass04@gmail.com>
2018-12-26 16:45:37 -0500

    Moving DB info to dotenv

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -1,16 +1,15 @@
 const { MongoClient } = require('mongodb')
 
-const { DB_USER, DB_PASS, DB_URL, DB_PORT, NODE_ENV } = process.env
+const { DB_USER, DB_PASS, DB_URL, DB_PORT, DB_NAME, NODE_ENV } = process.env
 // Connection url
 const dbUrl = `${DB_URL}:${DB_PORT}`
 const connectionString = NODE_ENV !== 'development' ? `mongodb://${DB_USER}:${DB_PASS}@${dbUrl}` : `mongodb://${dbUrl}`
-const dbName = 'kostumeKaraoke'
 
 const getCollection = async (colName, query) => {
   let client
   try {
     client = await MongoClient.connect(connectionString, { useNewUrlParser: true })
-    const db = client.db(dbName)
+    const db = client.db(DB_NAME)
     console.log('query', query)
     const test = await db.collection(colName).find(query || {}).toArray()
     console.log('test', test)
