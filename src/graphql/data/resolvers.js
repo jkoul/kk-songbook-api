9b15bbdbfe7a1b8c97cc454f52a54bcfdd6ecbc4
Michael Nasser <bignass04@gmail.com>
2019-05-19 11:00:07 -0400

    Initial commit for typescript prisma configuration

--- src/graphql/data/resolvers.js
+++ src/graphql/data/resolvers.js
@@ -3,13 +3,13 @@ const { getCollection, getSort, getQuery } = require('../../mongo')
 // Provide resolver functions for your schema fields
 module.exports = {
   Query: {
-    songs: async (root, { sort, ...args }) => {
+    songs: async (_, { sort, ...args }) => {
       const query = getQuery({ ...args })
       const sortBlock = getSort(sort)
 
       return await getCollection('songs', query, false, sortBlock)
     },
-    song: async (root, { id, ...args }) => {
+    song: async (_, { id, ...args }) => {
       const query = getQuery({ id, ...args })
       
       return await getCollection('songs', query, true)
--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,10 +1,14 @@
-const { ApolloServer } = require('apollo-server-express')
+const { GraphQLServer } = require('graphql-yoga')
 const schema = require('./data/schema')
 const resolvers = require('./data/resolvers')
+// const mocks = {
+//   Query: () => ({
+//     hello: () => 'Hello World',
+//     listOfStrings: () => new MockList([2, 6]),
+//   }),
+// }
 
-const server = new ApolloServer({
+module.exports = new GraphQLServer({
   typeDefs: schema,
   resolvers
 })
-
-module.exports = server
